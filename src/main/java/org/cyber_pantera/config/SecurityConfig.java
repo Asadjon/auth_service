27e1c0a0ca7305b38a17e215d8efa5fcb6dd899a
CyberPantera <asadjon4929@gmail.com>
2025-06-09 16:35:29 +0500

    add SecurityConfig

---
+++ src/main/java/org/cyber_pantera/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package org.cyber_pantera.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(csrfConfig -> {
+            try {
+                csrfConfig.disable()
+                        .authorizeHttpRequests(auth -> auth
+                                .requestMatchers("/**").permitAll()
+                                .anyRequest().authenticated())
+                        .sessionManagement(session -> session
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        .formLogin(AbstractHttpConfigurer::disable);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        return httpSecurity.build();
+    }
+}
